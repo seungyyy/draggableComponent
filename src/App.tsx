1431f04a478e8bbda60209794f3a5f0bf1e968aa
seungyeon.lee <dus3214@gmail.com>
2022-05-02 20:10:08 +0900

    style: 글로벌스타일 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Draggable } from './components/draggable';
+import GlobalStyles from './createGlobalStyle';
 
-
-function App() {
+const App: React.FC = () => {
   return (
     <>
+      <GlobalStyles />
+      <Draggable />
     </>
   );
 }
---
+++ src/createGlobalStyle.ts
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components"; 
+
+const globalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+  ol, ul, li {
+    list-style: none;
+  }
+`;
+
+export default globalStyle
